d6e5bd7d686a85073ed4599e26ec099a8bb13d85
Evgeny <evgeny.nov.00@gmail.com>
2024-11-17 17:48:08 +0300

    rewrite task

--- main.cpp
+++ main.cpp
@@ -6,18 +6,37 @@
 
 using namespace std;
 
+struct Vertie {
+    char name;
+    int mark;
+};
+
 class Graph {
 
 private:
     vector<vector<int>> matrix;
     vector<vector<int>> matrix_weight;
 
-    map<int, int> _vertices;
+    map<int, Vertie> _vertices;
 
 public:
     Graph() = default;
-    void ADD_V(int v, int mark = 0) {
-        _vertices[v] = mark;
+
+    int indexOfName(char c) {
+        for (const auto& [key, value] : _vertices) {
+            if (value.name == c) {
+                return key;
+            }
+        }
+        return -1;
+    }
+
+    char nameOfIndex(int i) {
+        return _vertices[i].name;
+    }
+
+    void ADD_V(char v, int mark = 0) {
+        _vertices[_vertices.size()] = {v, mark};
 
         matrix.resize(_vertices.size());
         for (auto& row : matrix) {
@@ -30,10 +49,10 @@ public:
         }
     }
 
-    void ADD_E(int start, int end, int weight = 1) {
-        //if(_vertices.find(start) != _vertices.end()) {
-        //    ADD_V(start);
-        //}
+    void ADD_E(char s, char e, int weight = 1) {
+        int start = indexOfName(s);
+        int end = indexOfName(e);
+
         if (start >= matrix.size() || end >= matrix.size()) {
             throw std::out_of_range("Invalid vertex index in ADD_E");
         }
@@ -42,72 +61,82 @@ public:
         matrix_weight[start][end] = weight;
     }
 
-    void DEL_V(int v) {
-        _vertices.erase(v);
+    void DEL_V(char v) {
+        int idx = indexOfName(v);
+        if (idx == -1) return;
 
-        matrix.erase(matrix.begin() + v);
+        _vertices.erase(idx);
+
+        matrix.erase(matrix.begin() + idx);
         for (auto& row : matrix) {
-            row.erase(row.begin() + v);
+            row.erase(row.begin() + idx);
         }
 
-        matrix_weight.erase(matrix_weight.begin() + v);
+        matrix_weight.erase(matrix_weight.begin() + idx);
         for (auto& row : matrix_weight) {
-            row.erase(row.begin() + v);
+            row.erase(row.begin() + idx);
         }
 
-        map<int, int> updated_vertices;
+        /*std::map<int, Vertie> updated_vertices;
+
         for (const auto& [key, value] : _vertices) {
-            updated_vertices[key > v ? key - 1 : key] = value;
+            int new_key = key > idx ? key - 1 : key;
+            updated_vertices[new_key] = value;
         }
-        _vertices = std::move(updated_vertices);
+        _vertices = std::move(updated_vertices);*/
     }
 
 
-    void DEL_E(int start, int end) {
+    void DEL_E(char s, char e) {
+        int start = indexOfName(s);
+        int end = indexOfName(e);
+
         matrix[start][end] = 0;
         matrix_weight[start][end] = 0;
     }
 
-    void EDIT_V(int v, int mark){
-        _vertices[v] = mark;
+    void EDIT_V(char v, int mark){
+        _vertices[indexOfName(v)] = {v, mark};
     }
 
-    void EDIT_E(int start, int end, int weight) {
+    void EDIT_E(char s, char e, int weight) {
+        int start = indexOfName(s);
+        int end = indexOfName(e);
         matrix_weight[start][end] = weight;
     }
 
-    int FIRST(int v) {
-        for (int i = 0; i < matrix[v].size(); ++i) {
-            if (matrix[v][i] == 1) {
+    int FIRST(char v) {
+        for (int i = 0; i < matrix[indexOfName(v)].size(); ++i) {
+            if (matrix[indexOfName(v)][i] == 1) {
                 return i;
             }
         }
         return -1;
     }
 
-    int NEXT(int v, int i) {
-        for (int j = i + 1; j < matrix[v].size(); ++j) {
-            if (matrix[v][j] == 1) {
+    int NEXT(char v, int i) {
+        for (int j = i + 1; j < matrix[indexOfName(v)].size(); ++j) {
+            if (matrix[indexOfName(v)][j] == 1) {
                 return j;
             }
         }
         return -1;
     }
 
-    int VERTEX(int v, int i) {
-        for (int j = 0; j < matrix[v].size(); ++j) {
-            if (matrix[v][j] == 1 && i == 0) {
+    int VERTEX(char v, int i) {
+        for (int j = 0; j < matrix[indexOfName(v)].size(); ++j) {
+            if (matrix[indexOfName(v)][j] == 1 && i == 0) {
                 return j;
             }
-            else if (matrix[v][j] == 1) i--;
+            else if (matrix[indexOfName(v)][j] == 1) i--;
         }
 
         return -1;
     }
     void print()  {
         std::cout << "Vertices:\n";
-        for (const auto& [vertex, mark] : _vertices) {
-            std::cout << "Vertex " << vertex << " (Mark: " << mark << ")\n";
+        for (const auto& [vertex, vertie] : _vertices) {
+            std::cout << "Vertex " << vertex << " (name: " << vertie.name << " mark: " << vertie.mark << ")\n";
         }
 
         // Вывод матрицы смежности
@@ -128,40 +157,43 @@ public:
             std::cout << "\n";
         }
     }
-    vector<vector<int>> getMatrix() const {
-        return matrix;
+    int size() const {
+        return matrix.size();
     }
 };
 
-void dfs(vector<int> path, Graph& graph, vector<vector<int>>& cycles){
-    auto matrix = graph.getMatrix();
+void dfs(char v, vector<int>& path, Graph& graph, vector<vector<int>>& cycles) {
+    // Получаем индекс первой смежной вершины
+    int w = graph.FIRST(v);
+    if (w == -1) return; // Если нет смежных вершин, возвращаемся
 
-    int v = path.back();
-    for (int i = 0; i < matrix.size(); ++i) {
-        if (matrix[v][i]) {
-            if (i == path[0]) { // Найден цикл
-                cycles.push_back(path);
-                cycles.back().push_back(i);
-            } else if (find(path.begin(), path.end(), i) == path.end()) {
-                vector<int> newPath = path;
-                newPath.push_back(i);
-                dfs(newPath, graph, cycles);
-            }
+    while (w != -1) {
+        if (w == path[0]) { // Найден цикл
+            path.push_back(w);
+            cycles.push_back(path);
+            path.pop_back(); // Убираем последний элемент для продолжения поиска
+        } else if (find(path.begin(), path.end(), w) == path.end()) { // Если вершина еще не в пути
+            path.push_back(w);
+            dfs(graph.nameOfIndex(w), path, graph, cycles);
+            path.pop_back(); // Убираем вершину после рекурсивного вызова
         }
+
+        // Переходим к следующей смежной вершине
+        w = graph.NEXT(v, w);
     }
-};
+}
 
 void task(Graph& graph) {
     vector<vector<int>> cycles;
 
-    auto matrix = graph.getMatrix();
-
-    for (int i = 0; i < matrix.size(); ++i) {
-        dfs({i}, graph, cycles);
+    for (int i = 0; i < graph.size(); ++i) { // Перебор всех вершин
+        vector<int> path = {i}; // Стартовый путь
+        dfs(graph.nameOfIndex(i), path, graph, cycles); // Запуск DFS для вершины i
     }
 
+    // Вывод результатов
     cout << "Количество циклов: " << cycles.size() << "\n";
-    cout << "Варианты обхода образующие циклы:\n";
+    cout << "Варианты обхода, образующие циклы:\n";
     for (const auto& cycle : cycles) {
         for (int v : cycle) {
             cout << v << " ";
@@ -170,44 +202,50 @@ void task(Graph& graph) {
     }
 }
 
+
 // Точка входа в программу
 int main() {
     Graph g;
 
-    /*g.ADD_V(0,0);
-    g.ADD_V(1,1);
-    g.ADD_V(2,2);
-    g.ADD_V(3,3);
-    g.ADD_V(4,4);
+    /*g.ADD_V('a',0);
+    g.ADD_V('b',1);
+    g.ADD_V('c',2);
+    g.ADD_V('d',3);
+    g.ADD_V('e',4);
 
-    g.ADD_E(0,1);
-    g.ADD_E(0,2);
-    g.ADD_E(1,3);
-    g.ADD_E(1,4);
-    g.ADD_E(2,4);
-    g.ADD_E(3,4);
-    g.ADD_E(4,1);
+    g.ADD_E('a','b');
+    g.ADD_E('a','c');
+    g.ADD_E('b','d');
+    g.ADD_E('b','e');
+    g.ADD_E('c','e');
+    g.ADD_E('d','e');
+    g.ADD_E('e','b');*/
 
-    task(g);*/
 
-    g.ADD_V(0,0);
-    g.ADD_V(1,1);
-    g.ADD_V(2,2);
-    g.ADD_V(3,3);
-    g.ADD_V(4,4);
-    g.ADD_V(5,5);
-    g.ADD_V(6,6);
 
-    g.ADD_E(0,1);
-    g.ADD_E(0,3);
-    g.ADD_E(1,3);
-    g.ADD_E(2,0);
-    g.ADD_E(3,4);
-    g.ADD_E(3,5);
-    g.ADD_E(3,0);
-    g.ADD_E(4,1);
-    g.ADD_E(4,2);
-    g.ADD_E(5,0);
+    g.ADD_V('a','a');
+    g.ADD_V('b','b');
+    g.ADD_V('c','c');
+    g.ADD_V('d','d');
+    g.ADD_V('e','e');
+    g.ADD_V('f','f');
+    g.ADD_V('j','j');
+
+    g.ADD_E('a','b');
+    g.ADD_E('a','d');
+    g.ADD_E('b','d');
+    g.ADD_E('c','a');
+    g.ADD_E('d','e');
+    g.ADD_E('d','f');
+    g.ADD_E('d','a');
+    g.ADD_E('e','b');
+    g.ADD_E('e','c');
+    g.ADD_E('f','a');
+
+
+    g.print();
 
     task(g);
+
+    return 0;
 }
